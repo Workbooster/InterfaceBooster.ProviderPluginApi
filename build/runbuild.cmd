f86731f11a9bd83cc0ab57401018fc85a81bf388
Roger Guillet <roger.guillet@workbooster.ch>
2014-12-02 16:34:50 +0100

    removed the pause at the end of the build batch file to be able to make a bulk-build over multiple projects

--- build/runbuild.cmd
+++ build/runbuild.cmd
@@ -4,5 +4,3 @@
 cls
 
 powershell -Command "& { [Console]::WindowWidth = 150; [Console]::WindowHeight = 50; Start-Transcript build.log; Import-Module ..\Tools\PSake\psake.psm1; Invoke-psake ..\Build\build.ps1 %*; Stop-Transcript; }"
-
-pause
