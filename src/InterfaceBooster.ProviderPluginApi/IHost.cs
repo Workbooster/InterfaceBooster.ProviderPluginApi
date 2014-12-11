8eaeff5423c165dea3541777066fb10390eacae4
Roger Guillet <roger.guillet@workbooster.ch>
2014-12-11 14:46:39 +0100

    added a property for the current frontend culter and wrote some xml-doc comments

--- src/InterfaceBooster.ProviderPluginApi/IHost.cs
+++ src/InterfaceBooster.ProviderPluginApi/IHost.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,8 +12,24 @@ namespace InterfaceBooster.ProviderPluginApi
     /// </summary>
     public interface IHost
     {
+        /// <summary>
+        /// Gets or sets the culture the user fronted uses.
+        /// </summary>
+        CultureInfo FrontendCulture { get; set; }
+
+        /// <summary>
+        /// Gets or sets the absolute path to the main directory of the interface definition currently is execued.
+        /// </summary>
         string CurrentInterfaceDefinitionDirectoryPath { get; set; }
+
+        /// <summary>
+        /// Gets or sets the absolute path to the main directory of the Interface Booster runtime (exe).
+        /// </summary>
         string RuntimeDirectoryPath { get; set; }
+
+        /// <summary>
+        /// Gets or sets the absolute path to the main directory of this provider plugin.
+        /// </summary>
         string ProviderPluginDiretcoryPath { get; set; }
     }
 }
