246f85e2c8b043619ee9974ec2ecb81c575e8cb8
Roger Guillet <roger.guillet@workbooster.ch>
2014-12-17 09:23:55 +0100

    Added a static Helper class that contains useful methods for provider plugin developers

---
+++ src/InterfaceBooster.ProviderPluginApi/Common/Helpers.cs
@@ -0,0 +1,23 @@
+﻿using InterfaceBooster.ProviderPluginApi.Communication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceBooster.ProviderPluginApi.Common
+{
+    public static class Helpers
+    {
+        public static T FindRequestByResourceName<T>(this IEnumerable<IRequest> listOfRequests, string resourceName) where T : IRequest
+        {
+            T request = (from r in listOfRequests
+                         where r is T
+                         && r.Resource != null
+                         && r.Resource.Name == resourceName
+                         select (T)r).FirstOrDefault();
+
+            return request;
+        }
+    }
+}
