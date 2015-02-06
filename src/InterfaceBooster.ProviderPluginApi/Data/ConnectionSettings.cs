3f32f5b830c970c5d68bc86bc48bd8edee05bb46
Roger Guillet <roger.guillet@workbooster.ch>
2015-02-06 14:28:47 +0100

    Added a helper method to ConnectionSettings for loading answer values.

--- src/InterfaceBooster.ProviderPluginApi/Data/ConnectionSettings.cs
+++ src/InterfaceBooster.ProviderPluginApi/Data/ConnectionSettings.cs
@@ -48,6 +48,38 @@ namespace InterfaceBooster.ProviderPluginApi.Data
             }
         }
 
+        /// <summary>
+        /// This helper method can be used to access an answer value by specifying the expected type and the full path of the question.
+        /// The <paramref name="questionFullPath"/> is composed by the Path and the Name of the Question separated by <paramref name="pathSeparator"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="questionFullPath">Is composed by the Path and the Name of the Question separated by <paramref name="pathSeparator"/>.</param>
+        /// <param name="defaultValue">The default return value if no answer was found.</param>
+        /// <param name="pathSeparator">The character used as separator in the <paramref name="questionFullPath"/>.</param>
+        /// <returns>The value of the answer or the <paramref name="defaultValue"/>.</returns>
+        public T GetAnswerValue<T>(string questionFullPath, T defaultValue = default(T), char pathSeparator = '.')
+        {
+            string[] fullPath = questionFullPath.Split(pathSeparator);
+
+            string[] path = fullPath.Take(fullPath.Length - 1).ToArray();
+            string name = fullPath.LastOrDefault();
+
+            Answer answer = (from a in _Answers
+                             where a.Question.Name == name
+                             && a.Question.ExpectedType == typeof(T)
+                             && a.Question.Path.SequenceEqual(path)
+                             select a).FirstOrDefault();
+
+            if (answer != null)
+            {
+                return (T)answer.Value;
+            }
+            else
+            {
+                return defaultValue;
+            }
+        }
+
         #endregion
     }
 }
---
+++ src/InterfaceBooster.Test.ProviderPluginApi/Data/ConnectionSettings_Test/Getting_Answers_Works.cs
@@ -0,0 +1,81 @@
+﻿using InterfaceBooster.ProviderPluginApi.Data;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceBooster.Test.ProviderPluginApi.Data.ConnectionSettings_Test
+{
+    [TestFixture]
+    public class Getting_Answers_Works
+    {
+        private ConnectionSettings _Settings;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            _Settings = new ConnectionSettings();
+
+            // add the test questions/answers
+
+            _Settings.Answers.Add(
+                new Answer(
+                    Question.New<string>("Test1", new string[] { "First", "Second" }, true)
+                    , "Test-Value-1"));
+
+            _Settings.Answers.Add(
+                new Answer(
+                    Question.New<string>("Test2", new string[] { "First", "Second" }, true)
+                    , "Test-Value-2"));
+
+            _Settings.Answers.Add(
+                new Answer(
+                    Question.New<string>("Test3", new string[] { "First", "Second" }, true)
+                    , "Test-Value-3"));
+
+            // add some other questions/answers with the same names but different paths and values
+            // this is to avoid chance hits
+
+            _Settings.Answers.Add(
+                new Answer(
+                    Question.New<string>("Test1", new string[] { "One", "Two" }, true)
+                    , "Different-Test-Value-1"));
+
+            _Settings.Answers.Add(
+                new Answer(
+                    Question.New<string>("Test2", new string[] { "One", "Two" }, true)
+                    , "Different-Test-Value-2"));
+
+            _Settings.Answers.Add(
+                new Answer(
+                    Question.New<string>("Test3", new string[] { "One", "Two" }, true)
+                    , "Different-Test-Value-3"));
+        }
+
+        [Test]
+        public void Getting_Answer_Works()
+        {
+            string testValue = _Settings.GetAnswerValue<string>("First.Second.Test2");
+
+            Assert.AreEqual("Test-Value-2", testValue);
+        }
+
+        [Test]
+        public void Getting_DefaultValue_Works()
+        {
+            string testValue = _Settings.GetAnswerValue<string>("Some.Wrong.Value.Test", "test-default");
+
+            Assert.AreEqual("test-default", testValue);
+        }
+
+        [Test]
+        public void Getting_Answer_With_Non_Default_PathSeparator_Works()
+        {
+            string testValue = _Settings.GetAnswerValue<string>("First_Second_Test2", pathSeparator: '_');
+
+            Assert.AreEqual("Test-Value-2", testValue);
+        }
+    }
+}
