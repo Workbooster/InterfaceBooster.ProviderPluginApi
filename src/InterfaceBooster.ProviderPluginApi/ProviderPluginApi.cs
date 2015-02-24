622bbfae4b65cbb9264e0ed78de765c1e5dbb953
Roger Guillet <roger.guillet@workbooster.ch>
2015-02-24 14:21:08 +0100

    added a list of CLR Types supported by Interface Booster

---
+++ src/InterfaceBooster.ProviderPluginApi/ProviderPluginApi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceBooster.ProviderPluginApi
+{
+    public static class ProviderPluginApi
+    {
+        /// <summary>
+        /// Gets a list of CLR Types supported by Interface Booster and Synery
+        /// </summary>
+        public static readonly IEnumerable<Type> SUPPORTED_TYPES = new Type[] { typeof(string), typeof(bool), typeof(int), typeof(decimal), typeof(double), typeof(char), typeof(DateTime) };
+    }
+}
