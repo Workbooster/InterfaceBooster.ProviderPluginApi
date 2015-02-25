1395c2fea5e4b7e565faec162e2c669212de298e
Roger Guillet <roger.guillet@workbooster.ch>
2015-02-25 09:30:06 +0100

    added a Regex for Synery identifiers

--- src/InterfaceBooster.ProviderPluginApi/ProviderPluginApi.cs
+++ src/InterfaceBooster.ProviderPluginApi/ProviderPluginApi.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace InterfaceBooster.ProviderPluginApi
@@ -12,5 +13,11 @@ namespace InterfaceBooster.ProviderPluginApi
         /// Gets a list of CLR Types supported by Interface Booster and Synery
         /// </summary>
         public static readonly IEnumerable<Type> SUPPORTED_TYPES = new Type[] { typeof(string), typeof(bool), typeof(int), typeof(decimal), typeof(double), typeof(char), typeof(DateTime) };
+
+        /// <summary>
+        /// Gets a regular expression instance that can be used to check for valid Synery Identifiers.
+        /// All identifers used in Synery (e.g. endpoint paths an names, field names etc.) must match this rules.
+        /// </summary>
+        public static readonly Regex SYNERY_IDENTIFIER_REGEX = new Regex(@"^[a-zA-Z_][a-zA-Z0-9_]*$");
     }
 }
