1a00e0fc1577db9afe560fc72598f3f0b9a660ec
Roger Guillet <roger.guillet@workbooster.ch>
2015-03-25 17:10:24 +0100

    added the filter types "ExactlyEqual" and "NotExactlyEqual" and made a few examples in the comments.

--- src/InterfaceBooster.ProviderPluginApi/Data/Filter/FilterTypeEnum.cs
+++ src/InterfaceBooster.ProviderPluginApi/Data/Filter/FilterTypeEnum.cs
@@ -8,8 +8,34 @@ namespace InterfaceBooster.ProviderPluginApi.Data.Filter
 {
     public enum FilterTypeEnum
     {
+        /// <summary>
+        /// Compares the values case in-sensitive.
+        /// e.g. "Test" == "test" is true
+        /// </summary>
         Equal,
+
+        /// <summary>
+        /// Compares the values case in-sensitive.
+        /// e.g. "Test" != "test" is false
+        /// </summary>
         NotEqual,
+
+        /// <summary>
+        /// Compares the values (case sensitive)
+        /// Examples:
+        /// "Test" === "test" is false
+        /// "Test" === "Test" is true
+        /// </summary>
+        ExactlyEqual,
+
+        /// <summary>
+        /// Compares the values (case sensitive)
+        /// Examples:
+        /// "Test" !== "test" is true
+        /// "Test" !== "Test" is false
+        /// </summary>
+        NotExactlyEqual,
+        
         GreaterThan,
         GreaterThanOrEqual,
         LessThan,
